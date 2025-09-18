146339829af955b18b8db5dea53e01207f8eb92d
JPG_Jose <josepqmg@gmail.com>
2025-09-18 16:20:19 +0100

    moved files around

--- boxes.py
+++ boxes.py
@@ -1,9 +1,9 @@
 class draggableArea:
     boxes = []
 
-    def __init__(self):
-        self.sizeX = 100
-        self.sizeY = 100
+    def __init__(self, x, y):
+        self.sizeX = x
+        self.sizeY = y
 
     def newBox(self, x, y):
         box = dragBox(x, y, self)
@@ -11,16 +11,20 @@ class draggableArea:
         print(f"I have a {box.sizeX}x{box.sizeY} box, the measurements are percentage.")
 
     def __str__(self):
-        return f"I am a {self.sizeX}x{self.sizeY} area. This string is useless now, all areas are 100x100, percentage."
+        return f"I am a {self.sizeX}x{self.sizeY} area."
 
 
 class dragBox:
-    def __init__(self, x, y, area):
+
+    def __init__(self, x, y, posX, posY, oX, oY, area):
         self.sizeX = x
         self.sizeY = y
-        self.posX = 0
-        self.posY = 0
+        self.posX = posX
+        self.posY = posY
+        self.orientationX = oX
+        self.orientationY = oY
         self.parentDrag = area
+
 
     def moveSelf(self, x, y):
         limitX = self.parentDrag.sizeX
